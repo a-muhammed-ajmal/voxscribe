2fb9a727302dda0152acabf99c780f04dfd1b631
a-muhammed-ajmal <ajmalconsults@gmail.com>
2026-02-26 15:16:23 +0400

    Update Firebase security rules documentation

--- firebase.js
+++ firebase.js
@@ -19,6 +19,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 const auth = getAuth(app);
+auth.settings.configValidation = false; // Disable config validation for localhost
 const db = getFirestore(app);
 const provider = new GoogleAuthProvider();
 
